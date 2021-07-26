ef60596ee07392f5d456fa4c6d1e2abe1ac31938
wangyang <isyang.wang@gmail.com>
2021-07-26 12:50:09 +0800

    fifth week

--- spring/my-starter/src/main/java/com/learn/starter/pojo/Student.java
+++ spring/my-starter/src/main/java/com/learn/starter/pojo/Student.java
@@ -3,10 +3,8 @@ package com.learn.starter.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
-@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student {
--- spring/spring-learn/src/main/java/com/pojo/Person.java
+++ spring/spring-learn/src/main/java/com/pojo/Person.java
@@ -3,7 +3,6 @@ package com.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 /**
  * @author wangyang
@@ -15,7 +14,6 @@ import lombok.ToString;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class Person {
 
   private Long id;
