0886e902ae9ea8839a77349835bc7dcb0a5eda40
wangyang <isyang.wang@gmail.com>
2021-07-26 12:39:52 +0800

    fifth week

--- spring/my-starter/src/main/java/com/learn/starter/pojo/Student.java
+++ spring/my-starter/src/main/java/com/learn/starter/pojo/Student.java
@@ -1,38 +1,18 @@
 package com.learn.starter.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Student(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Student() {
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
--- spring/spring-learn/src/main/java/com/pojo/Person.java
+++ spring/spring-learn/src/main/java/com/pojo/Person.java
@@ -1,5 +1,10 @@
 package com.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * @author wangyang
  * @ClassName Person
@@ -7,6 +12,10 @@ package com.pojo;
  * @Date 2021/7/22 下午9:39
  * @Version 1.0
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Person {
 
   private Long id;
@@ -17,60 +26,10 @@ public class Person {
 
   private Integer age;
 
-  public Person() {
-
-  }
 
   public Person(String name, String address, Integer age) {
     this(null, name, address, age);
   }
 
-  public Person(Long id, String name, String address, Integer age) {
-    this.id = id;
-    this.name = name;
-    this.address = address;
-    this.age = age;
-  }
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
-  public void setAge(Integer age) {
-    this.age = age;
-  }
-
-  @Override
-  public String toString() {
-    return "Person{" +
-        "id=" + id +
-        ", name='" + name + '\'' +
-        ", address='" + address + '\'' +
-        ", age=" + age +
-        '}';
-  }
 }
